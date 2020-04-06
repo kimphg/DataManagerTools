48f379540de28b08278971489f23b95ec75082fb
Phuong Phung <pkp013@gmail.com>
2020-04-06 14:56:30 +0700

    view user location

--- LocationSharingServer/LocationSharingServer/Form1.Designer.cs
+++ LocationSharingServer/LocationSharingServer/Form1.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(144, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(387, 119);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 252);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +60,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- LocationSharingServer/LocationSharingServer/Form1.cs
+++ LocationSharingServer/LocationSharingServer/Form1.cs
@@ -12,28 +12,59 @@ using System.Windows.Forms;
 
 namespace LocationSharingServer
 {
+    using System;
+    using System.Threading;
+
+    public class ServerListener
+    {
+        
+        public static string log = "";
+        public ServerListener()
+        {
+            
+        }
+        public static void Run()
+        {
+            IPEndPoint remoteEP;
+            UdpClient udpServer;
+            udpServer = new UdpClient(50000);
+            remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            
+            log += "server start ok";
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                byte[] data = udpServer.Receive(ref remoteEP); // listen to packet
+                byte[] dataLon = { data[11], data[10], data[9], data[8] };
+                byte[] dataLat = { data[15], data[14], data[13], data[12] };
+                float mlong = System.BitConverter.ToSingle(dataLon, 8);
+                float mlat = System.BitConverter.ToSingle(dataLat, 12);
+                //udpServer.Send(new byte[] { 1 }, 1, remoteEP); // reply back
+                log += "\n"+remoteEP.Address.ToString() + ", long:" + mlong.ToString() + ", lat:" + mlat.ToString();
+            }
+            
+        }
+    }
     public partial class Form1 : Form
     {
-        UdpClient udpServer;
-        IPEndPoint remoteEP;
+
+
+        Thread thread1;
         public Form1()
         {
             InitializeComponent();
-            UdpClient udpServer = new UdpClient(50000);
-            remoteEP = new IPEndPoint(IPAddress.Any,0);
-        }
-        private void  initNetwork()
-        {
+            thread1 = new Thread(ServerListener.Run);
+            thread1.Start();
             
-
-            while (true)
-            {
-                
-                var data = udpServer.Receive(ref remoteEP); // listen on port 11000
-                Console.Write("receive data from " + remoteEP.ToString());
-                udpServer.Send(new byte[] { 1 }, 1, remoteEP); // reply back
-            }
         }
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ServerListener.log;
+        }
+        
 
     }
 }
